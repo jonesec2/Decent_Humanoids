0d8e28dec4a84d1e746fa6a544a1db27130b5544
pb-totoro <jonesec2@gmail.com>
2021-01-01 16:32:19 -0500

    added blur to dice icon hover for desktop users as well as updated the box shadow element to be tiny and glow to be bigger

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -7,14 +7,21 @@ import icon from "../images/favicon.ico";
 
 
 function LandingPage() {
+
+   const [background, setBackground] = useState('App-header')
    const [path, setPath] = useState(window.location.pathname);
 
-   console.log(path)
+   // console.log(background)
+   // console.log(path)
 
    useEffect(() => {
       setPath(window.location.pathname);
       console.log(path + "test")
    }, [path]);
+
+   useEffect(() => {
+      console.log('background test')
+   }, [background])
 
    let history = useHistory();
 
@@ -26,20 +33,23 @@ function LandingPage() {
    return (
 
       // <Router basename="/decent_humanoids">
-      <div className="App">
-         <header className="App-header">
-            <div className="diceBackground"><Link to={"/home"}
-               onClick={event => history.push("/home")}></Link>
+      <div className='App'>
+         <header className={background}>
+            <div className="diceBackground">
+               <Link to={"/home"} onClick={event => history.push("/home")}></Link>
             </div>
-            <Link to={"/home"}
-               onClick={event => history.push("/home")}
-            ><img className="d20 color" width="47px" height="47px" src={icon} /></Link>
-            {/* <div className="diceBackground">
-               <Link to={"/home"}
-                  onClick={event => history.push("/home")}
-               ><img className="d20 color" width="47px" height="47px" src={icon} /></Link>
-            </div> */}
-            <div className="App"></div>
+            <Link
+               onMouseEnter={(e) => {
+                  setBackground('blur-App-header')
+               }}
+               onMouseLeave={(e) => {
+                  setBackground('App-header')
+               }}
+
+               to={"/home"} onClick={event => history.push("/home")}
+            >
+               <img className="d20 color" width="47px" height="47px" src={icon} />
+            </Link>
          </header>
          <Route exact path={"/home"} component={Home}>
             <Home />
