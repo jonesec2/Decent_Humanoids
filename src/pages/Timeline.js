c25cd354a964f6ce46193ff52e117289f54caf7a
pb-totoro <jonesec2@gmail.com>
2021-01-05 22:16:44 -0500

    timeline image added but need to figure out styling

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -15,12 +15,12 @@ export default function Timeline() {
          <Navbar />
          <div className="noMatch">
             <Wrapper>
-               <div className="minHome">
+               <div className="minHome timeline">
                   <h1 className="text-left">The story at a glance</h1>
                   <hr />
-                  <p>This is the Decent Humanoids history timeline </p>
+                  <img className="timelineImage" src={HistoryTest} />
 
-                  <hr></hr>
+
                   {/* <h2>Restart the adventure</h2>
                   <p>I'll try my best to get you what you're looking for. If I can't I'll try to get you as close as possible.</p>
                   <hr></hr>
