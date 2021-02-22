a2c5db479195df4060a2f32f3ee8b3541ebf130a
pb-totoro <jonesec2@gmail.com>
2021-02-22 14:25:47 -0500

    updating formating of code to be more readable

--- src/components/MainText.js
+++ src/components/MainText.js
@@ -31,108 +31,167 @@ export default function MainText(object) {
       <div className="container text-center mx-auto my-3">
          <div className="container mx-auto mb-3 pb-2 ml-5 border textR">
             <img onClick={e => showModal()} className="image" src={info.mainImage} alt={info.imageDescription} />
-            {show === 'true' ? (
-               <Modal img={info.mainImage} alt={info.imageDescription} show={show} showModal={showModal} />
-            ) : (
-                  <div></div>
-               )}
-            {info.rightInfo1 === "null" ? (
-               <div></div>
-            ) : (<p>{info.rightInfo1}</p>)}
+            {show === 'true' ?
+               (
+                  <Modal img={info.mainImage} alt={info.imageDescription} show={show} showModal={showModal} />
+               )
+               :
+               (<div></div>)
+            }
+            {info.rightInfo1 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.rightInfo1}</p>)
+            }
 
-            {info.rightInfo2 === "null" ? (
-               <div></div>
-            ) : (<p>{info.rightInfo2}</p>)}
+            {info.rightInfo2 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.rightInfo2}</p>)
+            }
 
-            {info.rightInfo3 === "null" ? (
-               <div></div>
-            ) : (<p>{info.rightInfo3}</p>)}
+            {info.rightInfo3 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.rightInfo3}</p>)
+            }
 
-            {info.rightInfo4 === "null" ? (
-               <div></div>
-            ) : (<p>{info.rightInfo4}</p>)}
+            {info.rightInfo4 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.rightInfo4}</p>)
+            }
 
-            {info.rightInfo5 === "null" ? (
-               <div></div>
-            ) : (<p>{info.rightInfo5}</p>)}
+            {info.rightInfo5 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.rightInfo5}</p>)
+            }
 
          </div>
          <div className="mx-auto mb-3 pb-2 endOverview">
             <p className="endOverview">{info.overview}</p>
          </div>
          <div className="border py-1 px-4 mr-4 textL row">
-            <button className="btn info col-12" id="firstSection"  onClick={e => object.sideBarScroll(e.target.id)}>{info.title1}</button>
+            <button className="btn info col-12" id="firstSection" onClick={e => object.sideBarScroll(e.target.id)}>{info.title1}</button>
             <button className="btn info col-12" id="secondSection" onClick={e => object.sideBarScroll(e.target.id)}>{info.title2}</button>
-            <button className="btn info col-12" id="thirdSection"  onClick={e => object.sideBarScroll(e.target.id)}>{info.title3}</button>
+            <button className="btn info col-12" id="thirdSection" onClick={e => object.sideBarScroll(e.target.id)}>{info.title3}</button>
          </div>
          <div className=" mx-auto my-3 py-2 startInfo">
             <hr></hr>
             <h3 ref={object.secondRef}>{info.title1}</h3>
-            {info.SectionOneP1 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionOneP1}</p>)}
-            {info.SectionOneP2 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionOneP2}</p>)}
 
-            {info.SectionOneP3 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionOneP3}</p>)}
+            {/* Section one Links */}
+            {info.subtitle1 === "null" ?
+               (<div></div>)
+               :
+               (
 
-            {info.SectionOneP4 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionOneP4}</p>)}
+                  <p>{info.subtitle1}</p>
 
-            {info.SectionOneP5 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionOneP5}</p>)}
+
+               )
+            }
+
+            {/*  */}
+
+            {/* Section 1 paragraphs */}
+            {info.SectionOneP1 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionOneP1}</p>)
+            }
+
+            {info.SectionOneP2 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionOneP2}</p>)
+            }
+
+            {info.SectionOneP3 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionOneP3}</p>)
+            }
+
+            {info.SectionOneP4 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionOneP4}</p>)
+            }
+
+            {info.SectionOneP5 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionOneP5}</p>)
+            }
 
 
 
             <hr></hr>
             <h3 ref={object.otherRef}>{info.title2}</h3>
-            {info.SectionTwoP1 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionTwoP1}</p>)}
-            {info.SectionTwoP2 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionTwoP2}</p>)}
+            {info.SectionTwoP1 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionTwoP1}</p>)
+            }
+            {info.SectionTwoP2 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionTwoP2}</p>)
+            }
 
-            {info.SectionTwoP3 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionTwoP3}</p>)}
+            {info.SectionTwoP3 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionTwoP3}</p>)
+            }
 
-            {info.SectionTwoP4 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionTwoP4}</p>)}
+            {info.SectionTwoP4 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionTwoP4}</p>)
+            }
 
-            {info.SectionTwoP5 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionTwoP5}</p>)}
+            {info.SectionTwoP5 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionTwoP5}</p>)
+            }
 
 
 
             <hr></hr>
 
             <h3 ref={object.lastRef}>{info.title3}</h3>
-            {info.SectionThreeP1 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionThreeP1}</p>)}
-            {info.SectionThreeP2 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionThreeP2}</p>)}
+            {info.SectionThreeP1 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionThreeP1}</p>)
+            }
+            {info.SectionThreeP2 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionThreeP2}</p>)
+            }
 
-            {info.SectionThreeP3 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionThreeP3}</p>)}
+            {info.SectionThreeP3 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionThreeP3}</p>)
+            }
 
-            {info.SectionThreeP4 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionThreeP4}</p>)}
+            {info.SectionThreeP4 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionThreeP4}</p>)
+            }
 
-            {info.SectionThreeP5 === "null" ? (
-               <div></div>
-            ) : (<p>{info.SectionThreeP5}</p>)}
+            {info.SectionThreeP5 === "null" ?
+               (<div></div>)
+               :
+               (<p>{info.SectionThreeP5}</p>)
+            }
 
          </div>
       </div>
